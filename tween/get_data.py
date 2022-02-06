f180363b67fcf841dbdd73a394d72ce56ea94595
anas-r-h <anas.rabhi.hakim@gmail.com>
2022-02-06 01:31:21 +0100

    add count tweets

--- tween/get_data.py
+++ tween/get_data.py
@@ -1,1 +1,23 @@
 import requests
+from credentials import TOKEN
+import json
+import pandas as pd
+
+def count_tweets(key_value: str):
+  key_value = key_value.replace(' ', '%20')
+  key_value = key_value.replace('#', '%23')
+
+  url = f"https://api.twitter.com/2/tweets/counts/recent?query={key_value}&granularity=hour"
+
+  payload={}
+  headers = {
+    'Authorization': f'Bearer {TOKEN}'
+  }
+
+  response = requests.request("GET", url, headers=headers, data=payload)
+  data = json.loads(response.text)['data']
+  #print(data)
+
+  return data
+
+count_tweets('ASMOL')
