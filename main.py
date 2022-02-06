0675ec7cf05bda5d2edad4f7a64e0038d0760b74
anas-rabhi <anas.rabhi.hakim@gmail.com>
2022-02-06 19:08:46 +0100

    add plot tweet counts

--- main.py
+++ main.py
@@ -1,5 +1,17 @@
 import pandas as pd
 import numpy as np
-import plotly as px
+import plotly.express as px
+import streamlit as st
+from tween import count_tweets
 
+if __name__ == '__main__':
+    with st.spinner('Wait for it...'):
+        #st.title('Twitter analyzer')
+        search_for = st.text_input('Subject')
 
+        if len(search_for)>0:
+            data = count_tweets(search_for)
+            fig = px.scatter(data, x="Date", y="Number of tweets",
+                       title="Number of tweets during last hour")
+            fig.update_layout(title_yanchor='middle')
+            st.plotly_chart(fig)
