09f743b3919732bc0521f40752b4014f83568be4
anas-rabhi <anas.rabhi.hakim@gmail.com>
2022-02-07 18:57:55 +0100

    update width

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ import streamlit as st
 from tween import *
 
 if __name__ == '__main__':
+    st.set_page_config(layout="wide")
+
     with st.spinner('Wait for it...'):
         st.title('Metrics by subject')
         search_for = st.text_input('Subject')
@@ -13,6 +15,7 @@ if __name__ == '__main__':
         granularity = st.radio("Granularity of the tweets",
                                ('Day', 'Hour', 'Minute'),
                                key='Day')
+
         if len(search_for)>0:
             # count number of tweets
             data = count_tweets(search_for, granularity)
@@ -23,16 +26,24 @@ if __name__ == '__main__':
 
             # add tweets data
             st.header('Advanced Metrics')
-            df = format_tweet_data(tweets_by_subject(subject=search_for, nb_max=4))
-            st.text(f'The following metrics are based on the most recent tweets during '
+            twts = st.slider('How much tweets do you want to load ? (hundreds)', 0, 15, 2)
+
+            df = format_tweet_data(tweets_by_subject(subject=search_for, nb_max=twts))
+            st.markdown(f'The following metrics are based on the most recent tweets during '
                     f'the last 7 days')
-            st.text(f'Number of tweets {df.shape[0]}')
-            col1, col2 = st.columns(2)
+            col1, col2, col3 = st.columns(3)
             col1.metric("Min Date", str(df.created_at.min())[:19])
             col2.metric("Max Date", str(df.created_at.max())[:19])
-            #col3.metric("Number of Tweets", df.shape[0])
+            col3.metric("Number of tweets", str(df.shape[0]))
 
             # This dataframe has 244 lines, but 4 distinct values for `day`
             type_tweets = round(df.referenced_tweets.value_counts(normalize=True), 2)
-            fig2 = px.pie(values=type_tweets, names=[(x.replace('_', ' ')).capitalize() for x in type_tweets.index])
-            st.plotly_chart(fig2)
+            device = round(df.source.value_counts(normalize=True), 2)
+
+            fig2 = px.pie(values=type_tweets, names=[(x.replace('_', ' ')).capitalize() for x in type_tweets.index], title='Type of tweet repartition')
+            fig3 = px.pie(values=device, names=[(x.replace('_', ' ')).capitalize() for x in device.index], title='Device repartition')
+
+            c1, c2 = st.columns(2)
+            c1.plotly_chart(fig2)
+            c2.plotly_chart(fig3)
+
