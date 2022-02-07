c96c55d6ff735ffa5987f866c8c196bc7780d0b2
anas-rabhi <anas.rabhi.hakim@gmail.com>
2022-02-07 18:25:39 +0100

    add recursive function for last tweets

--- tween/get_data.py
+++ tween/get_data.py
@@ -4,11 +4,11 @@ import json
 import pandas as pd
 import plotly.express as px
 
-def count_tweets(key_value: str):
-  key_value = key_value.replace(' ', '%20')
-  key_value = key_value.replace('#', '%23')
+def count_tweets(subject: str, granularity: str = 'hour'):
+  subject = subject.replace(' ', '%20')
+  subject = subject.replace('#', '%23')
 
-  url = f"https://api.twitter.com/2/tweets/counts/recent?query={key_value}&granularity=hour"
+  url = f"https://api.twitter.com/2/tweets/counts/recent?query={subject}&granularity={granularity.lower()}"
 
   payload={}
   headers = {
@@ -25,4 +25,38 @@ def count_tweets(key_value: str):
 
   return data
 
-count_tweets('ASMOL')
+def tweets_by_subject(subject: str, meta_token: str = '', granularity: str = 'hour', cnt: int = 1, nb_max = 2):
+  subject = subject.replace(' ', '%20')
+  subject = subject.replace('#', '%23')
+
+  if (cnt>=nb_max):
+    return []
+
+  if len(meta_token)!=0:
+    url = f"https://api.twitter.com/2/tweets/search/recent?max_results=100&next_token={meta_token}&tweet.fields=author_id,created_at,in_reply_to_user_id,source,attachments,conversation_id,public_metrics,referenced_tweets&query={subject}"
+  else:
+    url = f"https://api.twitter.com/2/tweets/search/recent?max_results=100&tweet.fields=author_id,created_at,in_reply_to_user_id,source,attachments,conversation_id,public_metrics,referenced_tweets&query={subject}"
+
+  payload = {}
+  headers = {
+    'Authorization': f'Bearer {TOKEN}'
+  }
+
+  response = requests.request("GET", url, headers=headers, data=payload)
+  if (response.status_code!=200):
+    return []
+
+
+  data = json.loads(response.text)['data']
+  meta = json.loads(response.text)['meta']
+
+  if ('next_token' in (c := meta)):
+    cnt += 1
+    new = tweets_by_subject(subject=subject,
+                                         meta_token=c['next_token'],
+                                         granularity=granularity,
+                                         cnt=cnt, nb_max=nb_max)
+    if len(new)>0:
+      data = data + new
+
+  return data
