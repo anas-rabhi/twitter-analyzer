153ac837e0c9b2fe997989ad1ef1f125c5d1812a
anas-rabhi <anas.rabhi.hakim@gmail.com>
2022-02-08 13:39:12 +0100

    add entities column

--- tween/get_data.py
+++ tween/get_data.py
@@ -29,13 +29,13 @@ def tweets_by_subject(subject: str, meta_token: str = '', granularity: str = 'ho
   subject = subject.replace(' ', '%20')
   subject = subject.replace('#', '%23')
 
-  if (cnt>int(nb_max)):
+  if (cnt > int(nb_max)):
     return []
 
   if len(meta_token)!=0:
-    url = f"https://api.twitter.com/2/tweets/search/recent?max_results=100&next_token={meta_token}&tweet.fields=author_id,created_at,in_reply_to_user_id,source,attachments,conversation_id,public_metrics,referenced_tweets&query={subject}"
+    url = f"https://api.twitter.com/2/tweets/search/recent?max_results=100&next_token={meta_token}&tweet.fields=author_id,created_at,entities,in_reply_to_user_id,source,attachments,conversation_id,public_metrics,referenced_tweets&query={subject}"
   else:
-    url = f"https://api.twitter.com/2/tweets/search/recent?max_results=100&tweet.fields=author_id,created_at,in_reply_to_user_id,source,attachments,conversation_id,public_metrics,referenced_tweets&query={subject}"
+    url = f"https://api.twitter.com/2/tweets/search/recent?max_results=100&tweet.fields=author_id,created_at,entities,in_reply_to_user_id,source,attachments,conversation_id,public_metrics,referenced_tweets&query={subject}"
 
   payload = {}
   headers = {
@@ -61,14 +61,22 @@ def tweets_by_subject(subject: str, meta_token: str = '', granularity: str = 'ho
 
   return data
 
-def format_tweet_data(data):
+def format_tweet_data(data) -> pd.DataFrame:
   df = pd.json_normalize(data)
-  df.referenced_tweets = df.referenced_tweets.apply(lambda x: x[0]['type'] if type(x) is list else 'tweet')
+  df = df[['author_id', 'referenced_tweets', 'conversation_id', 'source', 'text',
+       'created_at', 'id', 'public_metrics.retweet_count',
+       'public_metrics.reply_count', 'public_metrics.like_count',
+       'public_metrics.quote_count', 'entities.mentions']]
+  df['referenced_tweets'] = df['referenced_tweets'].apply(lambda x: x[0]['type'] if type(x) is list else 'tweet')
+  df['entities.mentions'] = df['entities.mentions'].apply(lambda x: x[0]['username'] if type(x) is list else 'User not found')
+  df.rename(columns={'entities.mentions': 'username'}, inplace=True)
   df.created_at = pd.to_datetime(df.created_at)
   df = df.drop_duplicates(subset=['created_at', 'author_id', 'text', 'id'])
   #print(df)
   return (df.reset_index())
 
+def get_count_reference_by_user(data: pd.DataFrame) -> pd.DataFrame:
+  data
 
 #format_tweet_data(tweets_by_subject('senegal'))
 #count_tweets('ASMOL')
